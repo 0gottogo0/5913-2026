ad3749de21f111602a40fd1d70103580fd799b53
0gottogo0 <0gottogo0@gmail.com>
2026-02-11 14:19:54 -0600

    small documentation changes

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
 
+	// Kraken X60
     private TalonFX hookPivot = new TalonFX(MotorID);
     private TalonFXConfiguration hootPivotConfig = new TalonFXConfiguration();
 
