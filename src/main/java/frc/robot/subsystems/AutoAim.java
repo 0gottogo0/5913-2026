bb174de2043b2892a052d23c4dd310012054bb28
0gottogo0 <0gottogo0@gmail.com>
2026-02-12 10:54:30 -0600

    climb distance logic

--- src/main/java/frc/robot/subsystems/AutoAim.java
+++ src/main/java/frc/robot/subsystems/AutoAim.java
@@ -35,6 +35,7 @@ public class AutoAim extends SubsystemBase {
     PoseEstimate LimelightClimbMessurement = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(LimelightClimb);
 
     double calculatedShot[] = {0.00, 0.00, 0.00, 0.00, 0.00};
+    double distanceFromClimb[] = {0.00, 0.00};
 
     State state = State.Goal;
 
@@ -128,6 +129,10 @@ public class AutoAim extends SubsystemBase {
         SmartDashboard.putNumber("Adjusted Target Top Shooter Speed", getShootOnMoveAimTarget()[3]);
         SmartDashboard.putNumber("Adjusted Target Time Of Flight", getShootOnMoveAimTarget()[4]);
         SmartDashboard.putNumberArray("Adjusted Target Data", getShootOnMoveAimTarget());
+
+        SmartDashboard.putNumber("Distance From Climb X", getClimbDistance()[0]);
+        SmartDashboard.putNumber("Distance From Climb Y", getClimbDistance()[1]);
+        SmartDashboard.putNumberArray("Distance From Climb Data", getClimbDistance());
     }
 
     /**
@@ -141,6 +146,11 @@ public class AutoAim extends SubsystemBase {
         robotSpeed = drivetrain.getState().Speeds;
     }
 
+    /**
+     * Are we on the blue aliance
+     * 
+     * @return true if on blue aliance
+     */
     public boolean isBlue() {
         try {
             if (DriverStation.getAlliance().get() == Alliance.Blue) {
@@ -177,7 +187,7 @@ public class AutoAim extends SubsystemBase {
      * move
      * 
      * @return An array with degrees, distance in meters,
-     *         bottom rps, top rps, and seconds
+     *         top rps, hood rps, and seconds
      */
     public double[] getShootOnMoveAimTarget() {
         // Code used for if we have a turret
@@ -186,13 +196,24 @@ public class AutoAim extends SubsystemBase {
         calculatedShot[0] = Math.atan(TurretRotatePointPose.minus(adjustedGoalPose).getX() / TurretRotatePointPose.minus(adjustedGoalPose).getY());
         // Gets distance
         calculatedShot[1] = Math.abs(Math.sqrt(Math.pow(TurretRotatePointPose.minus(adjustedGoalPose).getX(), 2) + Math.pow(TurretRotatePointPose.minus(adjustedGoalPose).getY(), 2)));
-        // Interpolates for bottom shooter
-        calculatedShot[2] = BottomShooterSpeedByDistance.get(calculatedShot[1]);
         // Interpolates for top shooter
-        calculatedShot[3] = TopShooterSpeedByDistance.get(calculatedShot[1]);
+        calculatedShot[2] = TopShooterSpeedByDistance.get(calculatedShot[1]);
+        // Interpolates for hood shooter
+        calculatedShot[3] = HoodShooterSpeedByDistance.get(calculatedShot[1]);
         // Interpolates time of flight
         calculatedShot[4] = TimeOfFlightByDistance.get(calculatedShot[1]);
         return calculatedShot;
+    }
+
+    /**
+     * Gets the distance from the climb
+     * 
+     * @return An Array with an X and Y
+     */    
+    public double[] getClimbDistance() {
+        distanceFromClimb[0] = robotPose.getX() - goalPose.getX();
+        distanceFromClimb[1] = robotPose.getY() - goalPose.getY();
+        return distanceFromClimb;
     }
 
     /**
